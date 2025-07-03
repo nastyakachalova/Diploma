536eb1240a3c7a28f5c07f70ffcad804d2c4a010
nastyakachalova <112856609+nastyakachalova@users.noreply.github.com>
2025-07-03 07:34:07 +0300

    Update settings.py

--- app/configs/settings.py
+++ app/configs/settings.py
@@ -3,13 +3,6 @@
 GRAPHML_FILENAME_DRIVE = "city_drive.graphml"
 GRAPHML_FILENAME_WALK = "city_walk.graphml"
 
-DB_CONFIG = {
-    "server": "localhost",
-    "database": "route_history",
-    "trusted_connection": True,
-    "driver": "ODBC Driver 17 for SQL Server"
-}
-
 CITIES = {
     "Краснодар": "Краснодар, Россия",
     "Москва": "Москва, Россия",
